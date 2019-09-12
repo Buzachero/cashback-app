8d1f5b2ea18372c9e2d586961688c49222f2fb17
Buzachero <henrique_buzachero@hotmail.com>
2019-09-11 23:00:25 -0300

    Enhance unit test for Disco Controller

--- src/test/java/com/buzachero/app/cashback/controller/DiscoControllerTest.java
+++ src/test/java/com/buzachero/app/cashback/controller/DiscoControllerTest.java
@@ -2,25 +2,29 @@ package com.buzachero.app.cashback.controller;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.buzachero.app.cashback.AbstractTest;
 import com.buzachero.app.cashback.domain.Disco;
 import com.buzachero.app.cashback.domain.enums.GeneroDisco;
+import com.buzachero.app.cashback.services.exceptions.ObjectNotFoundException;
 
-@Profile("test")
+@ActiveProfiles("test")
 public class DiscoControllerTest extends AbstractTest {
 	private static final String NOT_MATCHED = "%s diferentes!";
 	private static final String DISCO_ID = "Disco Id";
 	private static final String GENERO = "Genero";
 	private static final String NOME_DISCO = "Nome disco";
+	private static final String DISCO_BASE_URL_SUFFIX = "/discos/";
+	private Random rand = new Random();
 	
 	@Before
 	public void initializeMvc() {
@@ -46,75 +50,94 @@ public class DiscoControllerTest extends AbstractTest {
 	}
 
 	@Test
-	public void testFind_Disco1_POP() throws Exception {
-		String uri = "/discos/1";
-
+	public void testFind_Discos_POP() throws Exception {		
+		int discoId = rand.nextInt(50) + 1;
+		String uri = DISCO_BASE_URL_SUFFIX + discoId;
+		
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
 				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
 		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
+		assertEquals(HttpStatus.OK.value(), status);
 		String content = mvcResult.getResponse().getContentAsString();
 		Disco actualDisco = super.mapFromJson(content,  Disco.class);
 		
-		Disco expectedDisco = new Disco(1, "Disco 1", GeneroDisco.POP, 0.0);
+		Disco expectedDisco = new Disco(discoId, "Disco " + discoId, GeneroDisco.POP, 0.0);
 
 		assertNotNull(actualDisco);
 		assertDisco(expectedDisco, actualDisco);		
 	}
 	
 	@Test
-	public void testFind_Disco51_MPB() throws Exception {
-		String uri = "/discos/51";
+	public void testFind_Discos_MPB() throws Exception {
+		int discoId = rand.nextInt(50) + 51;
+		String uri = DISCO_BASE_URL_SUFFIX + discoId;
 
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
 				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
 		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
+		assertEquals(HttpStatus.OK.value(), status);
 		String content = mvcResult.getResponse().getContentAsString();
 		Disco actualDisco = super.mapFromJson(content,  Disco.class);
 		
-		Disco expectedDisco = new Disco(51, "Disco 51", GeneroDisco.MPB, 0.0);
+		Disco expectedDisco = new Disco(discoId, "Disco " + discoId, GeneroDisco.MPB, 0.0);
 
 		assertNotNull(actualDisco);
 		assertDisco(expectedDisco, actualDisco);		
 	}
 	
 	@Test
-	public void testFind_Disco101_CLASSIC() throws Exception {
-		String uri = "/discos/101";
+	public void testFind_Discos_CLASSIC() throws Exception {
+		int discoId = rand.nextInt(50) + 101;
+		String uri = DISCO_BASE_URL_SUFFIX + discoId;
 
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
 				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
 		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
+		assertEquals(HttpStatus.OK.value(), status);
 		String content = mvcResult.getResponse().getContentAsString();
 		Disco actualDisco = super.mapFromJson(content,  Disco.class);
 		
-		Disco expectedDisco = new Disco(101, "Disco 101", GeneroDisco.CLASSIC, 0.0);
+		Disco expectedDisco = new Disco(discoId, "Disco " + discoId, GeneroDisco.CLASSIC, 0.0);
 
 		assertNotNull(actualDisco);
 		assertDisco(expectedDisco, actualDisco);		
 	}
 
 	@Test
-	public void testFind_Disco151_ROCK() throws Exception {
-		String uri = "/discos/151";
+	public void testFind_Discos_ROCK() throws Exception {
+		int discoId = rand.nextInt(50) + 151;
+		String uri = DISCO_BASE_URL_SUFFIX + discoId;
 
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
 				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
 
 		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
+		assertEquals(HttpStatus.OK.value(), status);
 		String content = mvcResult.getResponse().getContentAsString();
 		Disco actualDisco = super.mapFromJson(content,  Disco.class);
 		
-		Disco expectedDisco = new Disco(151, "Disco 151", GeneroDisco.ROCK, 0.0);
+		Disco expectedDisco = new Disco(discoId, "Disco " + discoId, GeneroDisco.ROCK, 0.0);
 
 		assertNotNull(actualDisco);
 		assertDisco(expectedDisco, actualDisco);		
+	}
+	
+	@Test
+	public void testFind_Discos_Not_Exist() throws Exception {
+		int discoId = rand.nextInt(100) + 201;
+		String uri = DISCO_BASE_URL_SUFFIX + discoId;
+
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(HttpStatus.NOT_FOUND.value(), status);
+		
+		Exception exception = mvcResult.getResolvedException();
+		assertTrue(exception instanceof ObjectNotFoundException);		
 	}
 	
 	@Test
